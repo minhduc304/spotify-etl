a2af5100402f09f799de2daae44413ab69806db5
minhduc304 <mihducv42@gmail.com>
2025-04-26 23:42:40 +0700

    Added some more dags to clean up even more logs.

--- dags/cleanup_logs.py
+++ dags/cleanup_logs.py
@@ -18,15 +18,35 @@ dag = DAG(
     catchup=False,
 )
 
-# Find and remove log files older than 30 days
+# Find and remove log files older than specfied days
 cleanup_task = BashOperator(
     task_id='cleanup_airflow_logs',
-    bash_command='find /opt/airflow/logs -type f -name "*.log" -mtime +30 -delete',
+    bash_command='find ${LOGS_PATH} -type f -name "*.log" -mtime +30 -delete',
     dag=dag,
 )
 
 dbt_log_cleanup = BashOperator(
     task_id='cleanup_dbt_logs',
-    bash_command='find /opt/airflow/logs -name "dbt*.log" -mtime +15 -delete',
+    bash_command='find $LOGS_PATH -type f -name "dbt*.log" -mtime +15 -delete',
     dag=dag,
 )
+
+scheduler_cleanup = BashOperator(
+    task_id='cleanup_scheduler_logs',
+    bash_command='find $LOGS_PATH -type d -name "scheduler" -mtime +30 -delete',
+    dag=dag
+)
+
+processor_manager_cleanup = BashOperator(
+    task_id='cleanup_processor_manager_logs',
+    bash_command='find $LOGS_PATH -type d -name "dag_processor_manager" -mtime +30 -delete',
+    dag=dag
+)
+
+dag_runs_cleanup = BashOperator(
+    task_id='cleanup_dag_run_logs',
+    bash_command='find $LOGS_PATH -type d -name "dag_id*" -mtime +30 -delete',
+    dag=dag
+)
+
+
